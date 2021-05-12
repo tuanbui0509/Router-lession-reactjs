fb43108de88a2b4e833e0c3baffd0bc7ebe72c52
tuanbui0509 <tuanbui0509@gmail.com>
2021-05-12 16:19:02 +0700

    learn react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+export default class App extends Component {
+    render() {
+        return (
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <Router>
+                <div className="App">
+                    {/* Menu */}
+                    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                        <div className="collapse navbar-collapse" id="collapsibleNavId">
+                            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/">Trang chủ </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/contact">Liên hệ </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/about">Giới thiệu</Link>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </nav>
+                    {/* Nội dung */}
+                    <Route path='/' exact component={Home} />
+                    <Route path='/about' component={About} />
+                    <Route path='/contact' component={Contact} />
+                </div>
+            </Router>
+        )
+    }
 }
-
-export default App;
